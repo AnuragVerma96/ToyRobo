040af18cbae8c088fdb71378ebdff54579c0d0b7
Anurag Verma <85724585+AnuragVerma96@users.noreply.github.com>
2024-05-09 01:28:39 +0530

    Delete src/functions directory

--- src/functions/place.js
+++
@@ -1,8 +0,0 @@
-export const place = (x, y, f) => {
-  if (isReporting) setIsReporting(false);
-  if (!x && !y && !f) {
-    setPosition({ x: 0, y: 4 - 0, direction: getRandomDirection() });
-  } else {
-    setPosition({ x, y: 4 - y, direction: f });
-  }
-};
